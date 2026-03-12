96dbddd98b65dea093995ac52532f482c786549c
Arunihira <arunakoteda@gmail.com>
2026-03-13 00:07:16 +0530

    Added java day 1 program

--- java_git/java_day1.java
+++ java_git/java_day1.java
@@ -2,5 +2,13 @@ public class java_day1
 {
     public static void main(String[] args) {
         System.out.println("I am aruna");
+
+        System.out.println("I am a java developer");
+        System.out.println("I love java");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
     }
 }
