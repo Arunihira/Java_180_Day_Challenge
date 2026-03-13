e396913fbcac12615f79fd6bb8e38a4183b794dc
Arunihira <arunakoteda@gmail.com>
2026-03-13 11:28:09 +0530

    day 2 operators in java

---
+++ java_git/javaDay02_Operators.java
@@ -0,0 +1,17 @@
+public class javaDay02_Operators
+{
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println("Addition "+a+b);
+        //System.out.println("subtraction "+ a-b);
+        System.out.println("Multilication "+a*b);
+        System.out.println("Division "+a/b);
+
+
+
+
+        
+
+    }
+}
--- java_git/javaDay02_Variables.java
+++ java_git/javaDay02_Variables.java
@@ -1,14 +1,15 @@
-public class day02_Variables
+public class javaDay02_Variables
 {
     public static void main(String[] args) {
         //creating students details 
         int age=8;
         double height=4.5;
         char grade ='A';
-        boolean isStudent=true;
         System.out.println("age "+age);
         System.out.println("height "+height);
         System.out.println("grade "+grade);
+        
+
         
 
 
